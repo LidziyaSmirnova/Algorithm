c18bf5e03128866c35de3f5db15dfa3de980ca47
LidziyaSmirnova <32551960+LidziyaSmirnova@users.noreply.github.com>
2017-10-06 22:17:13 +0300

    the first commit

---
+++ Algorithm/src/SortByLineLength.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class StringLengthComparator implements Comparator<String> {
+	public int compare(String s1, String s2) { 
+		int length1 = s1.length();
+		int length2 = s2.length();
+		
+		if(length1>length2) {
+			return 1;
+		}
+		else if(length1<length2) {
+			return -1;
+		}
+			return 0;}
+	
+}
+public class SortByLineLength {
+
+	public static void main(String[] args) {
+	
+ ArrayList<String> greet = new ArrayList<String>();
+ 
+ greet.add("Hello");
+ greet.add("Hi");
+ greet.add("Добрый день!!");
+ greet.add("Привет");
+ greet.add("Hola");
+ 
+Collections.sort(greet, new StringLengthComparator());
+
+for(String greeting: greet){
+	 System.out.println(greeting);
+ }
+}
+}
